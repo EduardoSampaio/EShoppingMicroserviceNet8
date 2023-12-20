fc532a2c2c418e8de1a979b182362cebd5804e7a
Eduardo <edwardviana@gmail.com>
2023-12-20 18:28:01 -0300

    Add project files.

---
+++ Catalog.API/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Catalog.API;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; set; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if(env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseStaticFiles();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
+

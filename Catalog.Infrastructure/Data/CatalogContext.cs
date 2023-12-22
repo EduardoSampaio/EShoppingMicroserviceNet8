d4d20d75374c61bf325e49df8975ee45a804c30d
Eduardo <edwardviana@gmail.com>
2023-12-22 20:42:14 -0300

    repository

---
+++ Catalog.Infrastructure/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Core.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.Infrastructure.Data;
+public class CatalogContext : ICatalogContext
+{
+    public IMongoCollection<Product> Products { get; }
+
+    public IMongoCollection<ProductBrand> ProductBrands { get; }
+
+    public IMongoCollection<ProductType> ProductTypes { get; }
+
+    public CatalogContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+        
+    }
+}
---
+++ Catalog.Infrastructure/Data/ProductsContextSeed.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using System.Text.Json;
+
+namespace Catalog.Infrastructure.Data;
+
+public static class ProductsContextSeed
+{
+    public static void SeedData(IMongoCollection<Product> productCollection)
+    {
+        bool checkProduct = productCollection.Find(b => true).Any();
+        string path = Path.Combine("Data", "SeedData", "products.json");
+
+        if (!checkProduct)
+        {
+            var productData = File.ReadAllText(path);
+            var products = JsonSerializer.Deserialize<List<Product>>(productData);
+
+            if (products != null)
+            {
+                foreach (var product in products)
+                {
+                    productCollection.InsertOneAsync(product);
+                }
+            }
+        }
+    }
+}
+
+

0cc45f6f432397d83d303f03dd816f78c9433b51
Eduardo <edwardviana@gmail.com>
2024-01-08 21:47:17 -0300

    22. Startup Registration

--- Catalog.API/Startup.cs
+++ Catalog.API/Startup.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+﻿using Catalog.Application.Handlers;
+using Catalog.Core.Repositories;
+using Catalog.Infrastructure.Data;
+using MediatR;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 namespace Catalog.API;
 
@@ -14,11 +21,29 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
+        services.AddApiVersioning();
+        services.AddHealthChecks().AddMongoDb(Configuration["DatabaseSettings:ConnectionString"],
+          "Catalog Mongo Db Health Checks",
+          HealthStatus.Degraded);
+
+        services.AddSwaggerGen(c =>
+            c.SwaggerDoc("v1",
+            new OpenApiInfo()
+                {
+                    Title = "Catalog APi",
+                    Version = "v1",
+                }
+            ));
+
+        services.AddAutoMapper(typeof(Startup));
+        services.AddMediatR(typeof(CreateProductHandler).GetTypeInfo().Assembly);
+        services.AddScoped<ICatalogContext, CatalogContext>();
+        services.AddScoped<IProductRepository, ProductRepository>();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        if(env.IsDevelopment())
+        if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
         }
